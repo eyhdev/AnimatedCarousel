1ae26040b8369a814e0ecff006f0a947b9f58da6
Dev Light <106925514+developerlight@users.noreply.github.com>
2022-08-19 20:39:23 +0300

    Initial Commit

---
+++ AnimatedCarousel/AnimatedCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedCarouselApp.swift
+//  AnimatedCarousel
+//
+//  Created by Developer Light on 19.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
